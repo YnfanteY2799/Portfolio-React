1549bf62449869679f5811aa2d268df05bce95a5
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2022-07-19 07:01:02 -0400

    Update Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,3 +3,5 @@ import { BtnProps } from "../../utils/types/Router-Types";
 export default function Button({ title }: BtnProps) {
   return <button>{title}</button>;
 }
+
+// LATER ON https://v1.tailwindcss.com/components/buttons
